b38351b124eeead9d09c91a52cb7061c4f0627a2
Dorimanx <yuri@bynet.co.il>
2017-07-15 13:33:24 +0300

    patch-3.18.60-61
    Change-Id: Id5d8d45716be307ec40b00dcd7ab897e1872bc61

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -205,8 +205,12 @@ static const struct usb_device_id usb_quirk_list[] = {
 
 	/* INTEL VALUE SSD */
 	{ USB_DEVICE(0x8086, 0xf1a5), .driver_info = USB_QUIRK_RESET_RESUME },
-
-	{ }  /* terminating entry must be last */
+            
+	/* Hauppauge HVR-950q */
+	{ USB_DEVICE(0x2040, 0x7200), .driver_info =
+			USB_QUIRK_CONFIG_INTF_STRINGS },
+            
+    { }  /* terminating entry must be last */
 };
 
 static const struct usb_device_id usb_interface_quirk_list[] = {
